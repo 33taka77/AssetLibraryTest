0c06e6d50feb8227840540e06f0c9db5babfc5df
Aizawa Takashi <aizawatakashi@Aizawa-no-MacBook-Pro.local>
2014-03-10 13:45:00 +0900

    Initial Commit

---
+++ AssetLibraryTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AssetLibraryTest
+//
+//  Created by Aizawa Takashi on 2014/03/10.
+//  Copyright (c) 2014年 Aizawa Takashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
